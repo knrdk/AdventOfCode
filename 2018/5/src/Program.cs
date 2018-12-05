71c6d03065140ed46909038f51fdeaea28abf281
knrdk <konrad1991@gmail.com>
2018-12-05 07:01:19 +0100

    Day 5 part 1

---
+++ 2018/5/src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BigSmallLetterAsciiDifference = 32;
+            string filename = args[0];
+            StringBuilder polymer = File.ReadAllLines(filename).Aggregate(new StringBuilder(), (sb, x) => sb.Append(x));
+            
+            int i = 0;
+            while (i < polymer.Length - 1)
+            {                
+                int diff = Math.Abs(polymer[i] - polymer[i + 1]);
+                if (diff == BigSmallLetterAsciiDifference)
+                {
+                    polymer.Remove(i, 2);
+                    i = Math.Max(0, i - 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            int polymerLength = polymer.Length;
+            Console.WriteLine($"Polymer length: {polymerLength}");
+        }
+    }
+}
