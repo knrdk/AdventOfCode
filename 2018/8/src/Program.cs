89a4fe5b8f293eb7bb28d184e0cd7639a3b98c55
knrdk <konrad1991@gmail.com>
2018-12-08 09:29:53 +0100

    day 8 part 2

--- 2018/8/src/Program.cs
+++ 2018/8/src/Program.cs
@@ -17,6 +17,7 @@ namespace src
                 .ToList();
 
             Console.WriteLine(GetSumOfTreeMetadata(treeDescription.GetEnumerator()));
+            Console.WriteLine(GetTreeValue(treeDescription.GetEnumerator()));
         }
 
         static int GetSumOfTreeMetadata(IEnumerator<int> treeDescriptionEnumerator)
@@ -38,6 +39,39 @@ namespace src
                 result += treeDescriptionEnumerator.Current;
             }
 
+            return result;
+        }
+
+        static int GetTreeValue(IEnumerator<int> treeDescriptionEnumerator)
+        {
+            treeDescriptionEnumerator.MoveNext();
+            int numberOfSubtrees = treeDescriptionEnumerator.Current;
+            treeDescriptionEnumerator.MoveNext();
+            int numberOfMetadata = treeDescriptionEnumerator.Current;
+
+            int[] childValues = new int[numberOfSubtrees];
+            for (int i = 0; i < numberOfSubtrees; i++)
+            {
+                childValues[i] = GetTreeValue(treeDescriptionEnumerator);
+            }
+
+            int[] metadataValues = new int[numberOfMetadata];
+            for (int i = 0; i < numberOfMetadata; i++)
+            {
+                treeDescriptionEnumerator.MoveNext();
+                metadataValues[i] = treeDescriptionEnumerator.Current;
+            }
+
+            if (numberOfSubtrees == 0)
+            {                
+                return metadataValues.Sum();
+            }
+            
+            int result = 0;
+            foreach (var metadata in metadataValues)
+            {                
+                result += metadata-1 < numberOfSubtrees ? childValues[metadata-1] : 0;
+            }
             return result;
         }
     }
