df7d8d3959083f50f2cb79804002671b71fa44d7
knrdk <konrad1991@gmail.com>
2018-12-08 10:27:17 +0100

    2015 day 1

---
+++ 2015/1/src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            char[] input = File.ReadAllText(fileName).ToCharArray();
+
+            SolvePart1(input);
+            SolvePart2(input);
+        }
+
+        private static void SolvePart1(char[] input)
+        {
+            int result = input
+                .Select(x => x == '(' ? 1 : -1)
+                .Sum();
+            Console.WriteLine(result);
+        }
+
+        private static void SolvePart2(char[] input)
+        {
+            IEnumerable<int> directions = input.Select(x => x == '(' ? 1 : -1);
+            int i = 0;
+            int currentFloor = 0;
+            foreach (int direction in directions)
+            {
+                i++;
+                currentFloor += direction;
+                if (currentFloor == -1)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+        }
+    }
+}
