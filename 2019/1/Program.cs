011c0dc3dc08d8de24e5dba63212bdcca54a4e2e
knrdk <konrad1991@gmail.com>
2019-12-01 07:33:18 +0100

    2019-1 part2

--- 2019/1/Program.cs
+++ 2019/1/Program.cs
@@ -11,12 +11,32 @@ namespace _1
             string inputPath = args[0];
             Console.WriteLine($"Processing input file: {inputPath}");
 
-            string[] input = File.ReadAllLines(inputPath);
-            int solution = input
-                .Select(x=>int.Parse(x))
-                .Select(x=>x/3-2)
-                .Sum();
-            Console.WriteLine($"Solution: {solution}");
+            int[] input = File
+                .ReadAllLines(inputPath)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            Console.WriteLine($"Part 1 solution: {SolvePart1(input)}");
+            Console.WriteLine($"Part 2 solution: {SolvePart2(input)}");
+        }
+
+        static int SolvePart1(int[] input) => input
+            .Select(CalculateRequiredFuelForMass)
+            .Sum();
+
+        static int SolvePart2(int[] input) => input
+            .Select(CalculateTotalRequiredFuelForMass)
+            .Sum();
+
+        private static int CalculateRequiredFuelForMass(int mass) => mass / 3 - 2;
+
+        private static int CalculateTotalRequiredFuelForMass(int mass)
+        {
+            int requiredFuel = CalculateRequiredFuelForMass(mass);
+
+            return requiredFuel > 0
+                ? requiredFuel + CalculateTotalRequiredFuelForMass(requiredFuel)
+                : 0;
         }
     }
 }
