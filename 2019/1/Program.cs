3c501e0176731e980c1966ec853720b0c339f4f9
knrdk <konrad1991@gmail.com>
2019-12-01 07:05:05 +0100

    2019-1 part 1

---
+++ 2019/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = args[0];
+            Console.WriteLine($"Processing input file: {inputPath}");
+
+            string[] input = File.ReadAllLines(inputPath);
+            int solution = input
+                .Select(x=>int.Parse(x))
+                .Select(x=>x/3-2)
+                .Sum();
+            Console.WriteLine($"Solution: {solution}");
+        }
+    }
+}
