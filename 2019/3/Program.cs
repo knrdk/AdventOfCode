19ff29a9a30c96e29590d7eb1fbbee27bacf0a35
knrdk <konrad1991@gmail.com>
2019-12-03 07:34:21 +0100

    2019-3 part1

---
+++ 2019/3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = args[0];
+            Console.WriteLine($"Processing input file: {inputPath}");
+
+            var fileLines = File.ReadAllLines(inputPath);
+            var firstLineParsed = ParseInput(fileLines[0]);
+            var secondLineParsed = ParseInput(fileLines[1]);
+
+            var firstLinePoints = GetAllPoints(firstLineParsed);
+            var secondLinePoints = GetAllPoints(secondLineParsed);
+
+            var commonPoints = firstLinePoints.Intersect(secondLinePoints);
+            int solution = commonPoints.Select(x => Math.Abs(x.Item1) + Math.Abs(x.Item2)).OrderBy(x => x).First();
+            Console.WriteLine($"First part solution: {solution}");
+        }
+
+        static HashSet<(int, int)> GetAllPoints(IEnumerable<(int, Direction)> input)
+        {
+            HashSet<(int, int)> allPoints = new HashSet<(int, int)>();
+            (int, int) currentPoint = (0, 0);
+            foreach ((int, Direction) move in input)
+            {
+                for (int i = 0; i < move.Item1; i++)
+                {
+                    if (move.Item2 == Direction.Top)
+                    {
+                        currentPoint = (currentPoint.Item1, currentPoint.Item2 + 1);
+                    }
+                    else if (move.Item2 == Direction.Down)
+                    {
+                        currentPoint = (currentPoint.Item1, currentPoint.Item2 - 1);
+                    }
+                    else if (move.Item2 == Direction.Right)
+                    {
+                        currentPoint = (currentPoint.Item1 + 1, currentPoint.Item2);
+                    }
+                    else if (move.Item2 == Direction.Left)
+                    {
+                        currentPoint = (currentPoint.Item1 - 1, currentPoint.Item2);
+                    }
+
+                    allPoints.Add(currentPoint);
+                }
+            }
+            return allPoints;
+        }
+
+        static IEnumerable<(int, Direction)> ParseInput(string inputLine)
+        {
+            return inputLine
+                .Split(',')
+                .Select(ParseElement);
+        }
+
+        static (int, Direction) ParseElement(string element)
+        {
+            Direction direction = Direction.None;
+            if (element[0] == 'R')
+            {
+                direction = Direction.Right;
+            }
+            else if (element[0] == 'L')
+            {
+                direction = Direction.Left;
+            }
+            else if (element[0] == 'U')
+            {
+                direction = Direction.Top;
+            }
+            else if (element[0] == 'D')
+            {
+                direction = Direction.Down;
+            }
+
+            int value = int.Parse(element.Substring(1));
+
+            return (value, direction);
+        }
+    }
+
+    enum Direction
+    {
+        None,
+        Left,
+        Right,
+        Top,
+        Down
+    }
+}
