0f5aa3deb0e555850c3285231ec255872fb57bdd
knrdk <konrad1991@gmail.com>
2024-12-01 21:29:02 +0100

    Solved Day 1 of AoC 2024

---
+++ 2024/AoC/D01/Day01.cs
@@ -0,0 +1,66 @@
+namespace AoC.D01
+{
+    internal class Day01
+    {
+        private readonly string _inputFile;
+
+        public Day01(string inputFile)
+        {
+            _inputFile = inputFile;
+        }
+
+        public async Task<string> SolvePart1()
+        {
+            (var first, var second) = await ReadInput();
+
+            first.Sort();
+            second.Sort();
+
+            long result = first.Zip(second, (a, b) => Math.Abs(a - b)).Sum();
+            return result.ToString();
+        }
+
+        public async Task<string> SolvePart2()
+        {
+            (var first, var second) = await ReadInput();
+
+            Dictionary<int, int> histogram = CreateHistogram(second);
+            long result = 0;
+            foreach(int num in first){
+                int count = histogram.ContainsKey(num) ? histogram[num] : 0;
+                result += num * count;
+            }
+
+            return result.ToString();
+        }
+
+        private Dictionary<int, int> CreateHistogram(List<int> nums){
+            Dictionary<int, int> result = new();
+            foreach(int num in nums){
+                if(!result.ContainsKey(num)){
+                    result[num] = 0;
+                }
+
+                result[num]++;
+            }
+            return result;
+        }
+
+        private async Task<(List<int> first, List<int> second)> ReadInput()
+        {
+            string[] lines = await File.ReadAllLinesAsync(_inputFile);
+            List<int> first = new();
+            List<int> second = new();
+            foreach (string line in lines)
+            {
+                string[] numbersToParse = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int num1 = int.Parse(numbersToParse[0]);
+                int num2 = int.Parse(numbersToParse[1]);
+                first.Add(num1);
+                second.Add(num2);
+            }
+
+            return (first, second);
+        }
+    }
+}
---
+++ 2024/AoC/Program.cs
@@ -0,0 +1,14 @@
+﻿using AoC.D01;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello. Solving Aoc exercise!");
+
+string inputPath = @"C:\Users\knrdk\source\repos\AdventOfCode\2024\AoC\D01\input";
+
+Day01 solver = new(inputPath);
+
+string part1Solution = await solver.SolvePart1();
+Console.WriteLine($"Part1: {part1Solution}");
+
+string part2Solution = await solver.SolvePart2();
+Console.WriteLine($"Part2: {part2Solution}");
